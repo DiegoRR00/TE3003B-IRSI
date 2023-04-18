112881bd0bff3600adc1a8c44f0e8ff19bdb9cef
Diego Reyna <73494895+DiegoRR00@users.noreply.github.com>
2023-04-17 19:39:34 -0600

    Update main

--- main.py
+++ main.py
@@ -17,26 +17,29 @@ if __name__ == "__main__":
     #noise = 0.5 * np. random. normal (size-len (ts)) # Gaussian noise
     x_noise = x_t + noise
     # plot raw signal
-    plt.figure(figsize= (6.4, 2.4))
+    plt.figure(figsize= (12, 5))
     plt.plot(ts, x_t, alpha=0.8, lw=3, color="C1", label="Clean signal (ys)")
     plt.plot(ts, x_noise, color="C0", label="Noisy signal (x noise)")
     plt.xlabel("Time / 5")
     plt.ylabel("Amplitude")
     plt.legend(loc="lower center", bbox_to_anchor= [0.5, 11], ncol=2,fontsize="smaller")
-    plt.tight_layout()
+    #plt.tight_layout()
     plt.show()
+
     #Show the fourier transform
     xf, yf = fourier_transform(x_noise, sample_rate=fs,duration=5)
     plt.plot(xf,np.abs(yf))
     plt.show()
+
     # define lowpass filter with 2.5 Hz cutoff frequency
     fc = 10
     x_filtered = iir_filter(x_noise, fc, fs)
+
     # Signal Fourier Transform after filtering
     xf, yf = fourier_transform(x_filtered, sample_rate = fs, duration=5)
     plt.plot (xf, np.abs (yf))
     plt.show ()
-    plt.figure(figsize= [6.4, 2.4])
+    plt.figure(figsize= [12, 5])
     plt.plot(ts, x_noise, label="Raw signal")
     plt.plot (ts, x_filtered, alpha=0.8, lw=3, label="SciPy (filter)")
     plt.xlabel ("Time / s")
@@ -44,3 +47,15 @@ if __name__ == "__main__":
     plt. legend (loc="lower center", bbox_to_anchor = (0.5, 1), ncol=2,fontsize="smaller")
     plt.tight_layout()
     plt.show ()
+
+
+    # Apply filter foward and backward using filtfilt
+    x_filtered = iir_filter(x_noise, fc, fs, fbf = True)
+    plt.figure (figsize= (12, 5))
+    plt.plot(ts, x_noise, label="Raw signal")
+    plt.plot(ts, x_filtered, alpha=0.8, lw=3, label="filter forward and backward")
+    plt.xlabel("Time / s")
+    plt.ylabel("Amplitude")
+    plt.legend(loc="lower center", bbox_to_anchor= [0.5, 1], ncol=2, fontsize="smaller")
+    plt.tight_layout()
+    plt.show()

ffbd7f325b13abcb0532ba12a8f7ee9f3295164b
Diego Reyna <73494895+DiegoRR00@users.noreply.github.com>
2023-04-20 20:08:26 -0600

    Test

--- main.py
+++ main.py
@@ -1,34 +1,37 @@
-from filters import fir_filter, iir_filter, fourier_transform
-from scipy.signal import freqz
-from numpy import cos, sin, pi, absolute, arange
+# Diego Reyna Reyes
 import numpy as np
 from matplotlib import pyplot as plt
-if __name__ == "__main__":
+from numpy import pi, sin
 
+from filters import fir_filter, fourier_transform, iir_filter
+
+if __name__ == "__main__":
     # Create signal
-    np.random.seed (42) # for reproducibility
+    np.random.seed(42)  # for reproducibility
     # create time steps and corresponding sine wave with Gaussian noise
-    fs = 80 # sampling rate, Hz
-    ts = np.arange(0, 5, 1.0 / fs)
-    # time vector - 5 seconds
-    x_t = np.sin(2*np.pi * 1 * ts)
-    # signal @ 1.0 Hz, without noise
-    noise = 0.2*sin (2*pi *15.3*ts) + 0.1*sin(2*pi *16.7*ts + 0.1) + 0.1*sin (2*pi *23.45 *ts+.8)
-    #noise = 0.5 * np. random. normal (size-len (ts)) # Gaussian noise
+    fs = 80  # sampling rate, Hz
+    ts = np.arange(0, 5, 1.0 / fs)  # time vector - 5 seconds
+    x_t = np.sin(2 * np.pi * 1 * ts)  # signal @ 1.0 Hz, without noise
+    noise = (
+        0.2 * sin(2 * pi * 15.3 * ts)
+        + 0.1 * sin(2 * pi * 16.7 * ts + 0.1)
+        + 0.1 * sin(2 * pi * 23.45 * ts + 0.8)
+    )
+    # noise = 0.5 * np. random. normal (size-len (ts)) # Gaussian noise
     x_noise = x_t + noise
     # plot raw signal
-    plt.figure(figsize= (12, 5))
+    plt.figure(figsize=(12, 5))
     plt.plot(ts, x_t, alpha=0.8, lw=3, color="C1", label="Clean signal (ys)")
     plt.plot(ts, x_noise, color="C0", label="Noisy signal (x noise)")
     plt.xlabel("Time / 5")
     plt.ylabel("Amplitude")
-    plt.legend(loc="lower center", bbox_to_anchor= [0.5, 11], ncol=2,fontsize="smaller")
-    #plt.tight_layout()
+    plt.legend(loc="lower center", bbox_to_anchor=[0.5, 11], ncol=2, fontsize="smaller")
+    # plt.tight_layout()
     plt.show()
 
-    #Show the fourier transform
-    xf, yf = fourier_transform(x_noise, sample_rate=fs,duration=5)
-    plt.plot(xf,np.abs(yf))
+    # Show the fourier transform
+    xf, yf = fourier_transform(x_noise, sample_rate=fs, duration=5)
+    plt.plot(xf, np.abs(yf))
     plt.show()
 
     # define lowpass filter with 2.5 Hz cutoff frequency
@@ -36,38 +39,37 @@ if __name__ == "__main__":
     x_filtered = iir_filter(x_noise, fc, fs)
 
     # Signal Fourier Transform after filtering
-    xf, yf = fourier_transform(x_filtered, sample_rate = fs, duration=5)
-    plt.plot (xf, np.abs (yf))
-    plt.show ()
-    plt.figure(figsize= [12, 5])
+    xf, yf = fourier_transform(x_filtered, sample_rate=fs, duration=5)
+    plt.plot(xf, np.abs(yf))
+    plt.show()
+    plt.figure(figsize=[12, 5])
     plt.plot(ts, x_noise, label="Raw signal")
-    plt.plot (ts, x_filtered, alpha=0.8, lw=3, label="SciPy (filter)")
-    plt.xlabel ("Time / s")
-    plt.ylabel ("Amplitude")
-    plt. legend (loc="lower center", bbox_to_anchor = (0.5, 1), ncol=2,fontsize="smaller")
+    plt.plot(ts, x_filtered, alpha=0.8, lw=3, label="SciPy (filter)")
+    plt.xlabel("Time / s")
+    plt.ylabel("Amplitude")
+    plt.legend(loc="lower center", bbox_to_anchor=(0.5, 1), ncol=2, fontsize="smaller")
     plt.tight_layout()
-    plt.show ()
-
+    plt.show()
 
     # Apply filter foward and backward using filtfilt
-    x_filtered = iir_filter(x_noise, fc, fs, fbf = True)
-    plt.figure (figsize= (12, 5))
+    x_filtered = iir_filter(x_noise, fc, fs, fbf=True)
+    plt.figure(figsize=(12, 5))
     plt.plot(ts, x_noise, label="Raw signal")
     plt.plot(ts, x_filtered, alpha=0.8, lw=3, label="filter forward and backward")
     plt.xlabel("Time / s")
     plt.ylabel("Amplitude")
-    plt.legend(loc="lower center", bbox_to_anchor= [0.5, 1], ncol=2, fontsize="smaller")
+    plt.legend(loc="lower center", bbox_to_anchor=[0.5, 1], ncol=2, fontsize="smaller")
     plt.tight_layout()
     plt.show()
 
     # FIR Filter
     nyq_rate = fs / 2.0
-    x_filtered_fir,_,_ = fir_filter(x_noise, fs, fc)
-    plt.figure(figsize= (12, 5))
+    x_filtered_fir, _, _ = fir_filter(x_noise, fs, fc)
+    plt.figure(figsize=(12, 5))
     plt.plot(ts, x_noise, label="Raw signal")
     plt.plot(ts, x_filtered_fir, alpha=0.8, lw=3, label="FIR filter")
-    plt.xlabel ("Time / s")
-    plt.ylabel ("Amplitude")
-    plt. legend (loc="lower center", bbox_to_anchor= [0.5, 1], ncol=2,fontsize="smaller")
+    plt.xlabel("Time / s")
+    plt.ylabel("Amplitude")
+    plt.legend(loc="lower center", bbox_to_anchor=[0.5, 1], ncol=2, fontsize="smaller")
     plt.tight_layout()
     plt.show()
